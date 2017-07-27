3a0a8f0d195f4bb31e34a3727cd2d45ceb50519a
gcampuzano14 <gcampuzano14@outlook.com>
2017-07-27 09:12:18 -0500

    [master] Include initial file renaming script

---
+++ renamer.py
@@ -0,0 +1,24 @@
+import os
+import re
+import easygui as eg
+
+def repl(m):
+    inner_word = m.group(1)
+    
+    return "".join(['_0',inner_word,'_'])
+
+msg=""
+title="Select root directory"
+default=os.path.abspath("D:\Dropbox\CAMPECHA_SHARE\PORTUGUES\Michel Thomas")
+root_path = eg.diropenbox(msg, title, default)
+print root_path
+
+dirs = next(os.walk(root_path))[1]
+print dirs
+
+for d in dirs:
+    files = os.listdir(os.path.join(root_path,d))
+    for f in files:
+        sub_str = re.sub(r'_(\d{1})_', repl, f)
+        print sub_str
+        os.rename(os.path.join(root_path,d,f), os.path.join(root_path,d,sub_str))
