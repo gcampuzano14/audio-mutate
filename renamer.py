8c870d73b47c91999df82761e73faf613cc6f117
gcampuzano14 <gcampuzano14@outlook.com>
2017-07-27 09:22:27 -0500

    [master] Linting

--- renamer.py
+++ renamer.py
@@ -2,23 +2,23 @@ import os
 import re
 import easygui as eg
 
+
 def repl(m):
     inner_word = m.group(1)
-    
-    return "".join(['_0',inner_word,'_'])
+    return "".join(['_0', inner_word, '_'])
 
-msg=""
-title="Select root directory"
-default=os.path.abspath("D:\Dropbox\CAMPECHA_SHARE\PORTUGUES\Michel Thomas")
+msg = ""
+title = "Select root directory"
+default = os.path.abspath("PATHTOFILE_HERE")
 root_path = eg.diropenbox(msg, title, default)
-print root_path
-
+print(root_path)
 dirs = next(os.walk(root_path))[1]
-print dirs
+print(dirs)
 
 for d in dirs:
-    files = os.listdir(os.path.join(root_path,d))
+    files = os.listdir(os.path.join(root_path, d))
     for f in files:
         sub_str = re.sub(r'_(\d{1})_', repl, f)
-        print sub_str
-        os.rename(os.path.join(root_path,d,f), os.path.join(root_path,d,sub_str))
+        print(sub_str)
+        os.rename(os.path.join(root_path, d, f),
+                  os.path.join(root_path, d, sub_str))
